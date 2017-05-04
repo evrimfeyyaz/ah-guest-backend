59a806afffd8bcd2b667d3ac778a4db749cd5c79
Evrim P <evrimfeyyaz@gmail.com>
2017-05-04 18:13:56 +0300

    Fix wrong route definitions and properly load the RoomService namespace in the room service categories controller.

--- app/controllers/v0/room_service/categories_controller.rb
+++ app/controllers/v0/room_service/categories_controller.rb
@@ -1,6 +1,6 @@
 class V0::RoomService::CategoriesController < ApplicationController
   def index
-    @categories = RoomService::Category.all
+    @categories = ::RoomService::Category.all
 
     render json: @categories
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  namespace :v0 do
-    namespace :room_service do
-      get 'categories/index'
-    end
-  end
-
-  namespace :room_service do
-    get 'categories/index'
-  end
-
   namespace :v0, defaults: { format: :json } do
     namespace :room_service, path: 'room-service' do
       resources :categories, only: [:index]
