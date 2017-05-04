2dc7e9c3c75af6bfd1ccf65096aba742c0ba4c46
Evrim P <evrimfeyyaz@gmail.com>
2017-05-04 18:14:29 +0300

    Add some room service categories to the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+RoomService::Category.destroy_all
+
+RoomService::Category.create!([{
+  title: "Breakfast",
+  description: "Served from 6am to 11am"
+                              }, {
+  title: "Breakfast A La Carte",
+  description: "Served from 6am to 11am"
+                              }, {
+  title: "Starters and Salads"
+                              }, {
+  title: "Arabic Mezzeh Selection"
+                              }, {
+  title: "Home-Made Soups"
+                              }, {
+  title: "Main Fares"
+                              }, {
+  title: "From The Grill"
+                              }, {
+  title: "Pasta"
+                              }, {
+  title: "Pizza Corner"
+                              }, {
+  title: "Burgers & Wraps"
+                              }, {
+  title: "Kids Choice"
+                              }, {
+  title: "Our Pastry Chef's Delights"
+                              }, {
+  title: "Round O'Clock Menu - 24/7"
+                              }, {
+  title: "Beverages Hot & Cold",
+  description: "Available 24 hours"
+                              }
+])
