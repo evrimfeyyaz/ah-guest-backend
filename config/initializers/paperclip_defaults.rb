db49dbebb01d2375979b8990153f6a1a30023d92
Evrim P <evrimfeyyaz@gmail.com>
2017-05-07 16:11:11 +0300

    Set the https protocol for S3 correctly (one more try).

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -6,11 +6,11 @@ Paperclip::Attachment.default_options.update(
 
 if Rails.env.production?
   Paperclip::Attachment.default_options.merge!(storage: :s3,
+                                               s3_protocol: :https,
                                                s3_credentials: {
                                                  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                                  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
                                                  s3_region: ENV['AWS_REGION'],
-                                                 bucket: 'dry-dawn-66033-2',
-                                                 s3_protocol: 'https'
+                                                 bucket: 'dry-dawn-66033-2'
                                                })
 end
