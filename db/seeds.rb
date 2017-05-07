2e415588c8cf790dd7c97bc0450c541fc308d2e7
Evrim P <evrimfeyyaz@gmail.com>
2017-05-07 11:46:51 +0300

    Add more seed images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,35 +3,45 @@ RoomService::Category.destroy_all
 RoomService::Category.create!([{
   title: "Breakfast",
   description: "Served from 6am to 11am",
-  image: Rails.root.join('seed/breakfast@3x.jpg').open
+  image: Rails.root.join('seed/breakfast.jpg').open
                               }, {
   title: "Breakfast A La Carte",
-  description: "Served from 6am to 11am"
+  description: "Served from 6am to 11am",
+  image: Rails.root.join('seed/breakfast-a-la-carte.jpg').open
                               }, {
-  title: "Starters and Salads"
+  title: "Starters and Salads",
+  image: Rails.root.join('seed/starters-and-salads.jpg').open
                               }, {
   title: "Arabic Mezzeh Selection"
                               }, {
-  title: "Home-Made Soups"
+  title: "Homemade Soups",
+  image: Rails.root.join('seed/homemade-soups.jpg').open
                               }, {
   title: "Main Fares"
                               }, {
-  title: "From The Grill"
+  title: "From The Grill",
+  image: Rails.root.join('seed/from-the-grill.jpg').open
                               }, {
-  title: "Pasta"
+  title: "Pasta",
+  image: Rails.root.join('seed/pasta.jpg').open
                               }, {
-  title: "Pizza Corner"
+  title: "Pizza Corner",
+  image: Rails.root.join('seed/pizza-corner.jpg').open
                               }, {
-  title: "Burgers & Wraps"
+  title: "Burgers & Wraps",
+  image: Rails.root.join('seed/burgers-and-wraps.jpg').open
                               }, {
   title: "Kids Choice"
                               }, {
-  title: "Our Pastry Chef's Delights"
+  title: "Our Pastry Chef's Delights",
+  image: Rails.root.join('seed/our-pastry-chefs-delights.jpg').open
                               }, {
-  title: "Round O'Clock Menu - 24/7"
+  title: "Round O'Clock Menu - 24/7",
+  image: Rails.root.join('seed/round-o-clock-menu-24-7.jpg').open
                               }, {
   title: "Beverages Hot & Cold",
-  description: "Available 24 hours"
+  description: "Available 24 hours",
+  image: Rails.root.join('seed/beverages-hot-and-cold.jpg').open
                               }
 ])
 
