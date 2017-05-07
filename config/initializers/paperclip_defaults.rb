31d25cbd32995c0dce13ad3d9f172e9ebdd88001
Evrim P <evrimfeyyaz@gmail.com>
2017-05-07 14:07:39 +0300

    Change the image upload directory once again, and ignore that directory to not clutter up the repo.

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,5 +1,5 @@
 Paperclip::Attachment.default_options.update(
-  url: '/:class/:attachment/:id_partition/:style/:hash.:extension',
+  url: '/system/:class/:attachment/:id_partition/:style/:hash.:extension',
   hash_secret: Rails.application.secrets.secret_key_base,
   default_url: ''
 )
