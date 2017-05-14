ffa742cff216c07c69c19cfd47f80e3ffad71e9e
Evrim P <evrimfeyyaz@gmail.com>
2017-05-14 16:02:21 +0300

    Add price and title validations for item option choice.

--- app/models/room_service/item_option_choice.rb
+++ app/models/room_service/item_option_choice.rb
@@ -1,3 +1,6 @@
 class RoomService::ItemOptionChoice < ApplicationRecord
   belongs_to :option, foreign_key: :room_service_item_option_id, class_name: 'RoomService::ItemOption'
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/room_service/item_option_choice_spec.rb
+++ spec/models/room_service/item_option_choice_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RoomService::ItemOptionChoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe RoomService::ItemOptionChoice, 'Validations' do
+  it { should validate_presence_of :title }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
 end
