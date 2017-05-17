4e749c7361ad17c9faf17412390124613e872e3b
Evrim P <evrimfeyyaz@gmail.com>
2017-05-17 19:41:50 +0300

    Add some more validations to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, :first_name, :last_name, presence: true
+  validates_presence_of :first_name
+  validates_length_of   :first_name, maximum: 60
+
+  validates_presence_of :last_name
+  validates_length_of   :last_name, maximum: 60
+
+  validates_presence_of   :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of     :email, with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\z/i
+
+  validates_length_of :password, minimum: 8, maximum: 128, on: :create
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,29 @@ require 'rails_helper'
 
 describe User, 'Validations' do
   it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should allow_values('john@example.com',
+                           'john.doe@example.com',
+                           'j@example.com',
+                           'john@example.verylongtld').for(:email) }
+  it { should_not allow_values('',
+                               '@',
+                               '@.',
+                               '@example.com',
+                               'www.example.com',
+                               '.',
+                               '.com',
+                               'john doe@example.com',
+                               'john@example',
+                               'john@example.',
+                               'john@example.a').for(:email) }
+
   it { should have_secure_password }
+  it { should validate_length_of(:password).is_at_least(8).is_at_most(128).on(:create) }
+
   it { should validate_presence_of :first_name }
+  it { should validate_length_of(:first_name).is_at_most(60) }
+
   it { should validate_presence_of :last_name }
+  it { should validate_length_of(:last_name).is_at_most(60) }
 end
