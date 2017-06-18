5c7c7f2e97b38ff497fb3cc9994b3cd296a7bd06
Evrim P <evrimfeyyaz@gmail.com>
2017-06-18 14:52:42 +0300

    Change the name of the environment variable that stores the name of the S3 Bucket.

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -11,6 +11,6 @@ if Rails.env.production?
                                                  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                                  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
                                                  s3_region: ENV['AWS_REGION'],
-                                                 bucket: ENV['AWS_BUCKET']
+                                                 bucket: ENV['S3_BUCKET_NAME']
                                                })
 end
