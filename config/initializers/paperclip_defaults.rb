3ab7ed09a65bd3fd4b7c8e96eff2c04d2cf71582
Evrim P <evrimfeyyaz@gmail.com>
2017-06-18 14:51:32 +0300

    Fetch the AWS Bucket name from an environment variable.

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -11,6 +11,6 @@ if Rails.env.production?
                                                  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                                  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
                                                  s3_region: ENV['AWS_REGION'],
-                                                 bucket: 'dry-dawn-66033-2'
+                                                 bucket: ENV['AWS_BUCKET']
                                                })
 end
