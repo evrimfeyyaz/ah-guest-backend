57797ffb73c929ab4bcc87f18eda96ff9fc352f1
Evrim P <evrimfeyyaz@gmail.com>
2017-06-19 10:06:44 +0300

    Take out SendGrid configuration.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,5 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
-  devise :database_authenticatable, :confirmable, :timeoutable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable, :rememberable
 end
