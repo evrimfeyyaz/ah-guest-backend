915c3444f10df85b5e4663c612fb30d7a94cb625
Evrim P <evrimfeyyaz@gmail.com>
2017-06-19 10:53:07 +0300

    Fix heroku

--- app/controllers/admin/room_service/orders_controller.rb
+++ app/controllers/admin/room_service/orders_controller.rb
@@ -1,34 +1,36 @@
-class Admin::RoomService::OrdersController < Admin::BaseController
-  def index
-    @orders = RoomService::Order.order(created_at: :desc)
-  end
-
-  def show
-    @order = RoomService::Order.find(params[:id])
-  end
-
-  def complete
-    @order = RoomService::Order.find(params[:id])
-
-    if reservation_params[:room_number].present?
-      @order.reservation.room_number = reservation_params[:room_number]
-      @order.reservation.save
+class Admin
+  class RoomService::OrdersController < Admin::BaseController
+    def index
+      @orders = RoomService::Order.order(created_at: :desc)
     end
 
-    if @order.reservation.room_number.blank?
-      render 'admin/room_service/orders/room_number_entry' and return
+    def show
+      @order = RoomService::Order.find(params[:id])
     end
 
-    @order.status = 1
+    def complete
+      @order = RoomService::Order.find(params[:id])
 
-    if @order.save
-      redirect_to admin_room_service_orders_url
+      if reservation_params[:room_number].present?
+        @order.reservation.room_number = reservation_params[:room_number]
+        @order.reservation.save
+      end
+
+      if @order.reservation.room_number.blank?
+        render 'admin/room_service/orders/room_number_entry' and return
+      end
+
+      @order.status = 1
+
+      if @order.save
+        redirect_to admin_room_service_orders_url
+      end
     end
-  end
 
-  private
+    private
 
-  def reservation_params
-    params.permit(:room_number)
+    def reservation_params
+      params.permit(:room_number)
+    end
   end
 end
