6d9110801c0139fa7598e1eb5adc737941b9a327
Evrim P <evrimfeyyaz@gmail.com>
2017-07-19 17:31:34 +0300

    Refactor the room service categories controller, untrack some IDEA files.

--- app/controllers/api/v0/room_service/categories_controller.rb
+++ app/controllers/api/v0/room_service/categories_controller.rb
@@ -2,8 +2,21 @@ class Api::V0::RoomService::CategoriesController < ApiController
   skip_before_action :authenticate_user_by_auth_token, only: [:index]
 
   def index
-    categories = ::RoomService::Category.all
+    load_categories
+    render_categories_json
+  end
 
-    render json: categories
+  private
+
+  def load_categories
+    @categories ||= categories_scope.to_a
+  end
+
+  def categories_scope
+    RoomService::Category.all
+  end
+
+  def render_categories_json
+    render json: @categories
   end
 end
