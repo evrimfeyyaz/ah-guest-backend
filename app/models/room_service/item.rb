b24bff2e4b2484d1a43857a94d76867efe8e45c8
Evrim P <evrimfeyyaz@gmail.com>
2017-09-13 09:22:24 +0300

    Add delegate tests, and destroy dependent association models in the Item model.

--- app/models/room_service/item.rb
+++ app/models/room_service/item.rb
@@ -2,11 +2,11 @@ class RoomService::Item < ApplicationRecord
   belongs_to :category_section, foreign_key: 'room_service_category_section_id',
              counter_cache: 'room_service_items_count', inverse_of: :items, class_name: 'RoomService::Category::Section'
   has_many :tag_associations, inverse_of: :item, class_name: 'RoomService::Item::TagAssociation',
-           foreign_key: 'room_service_item_id'
+           foreign_key: 'room_service_item_id', dependent: :destroy
   has_many :tags, through: :tag_associations
-  has_many :choice_associations, inverse_of: :item, class_name: 'RoomService::Item::ChoiceAssociation',
-           foreign_key: 'room_service_item_id'
-  has_many :choices, through: :choice_associations
+  has_many :item_choice_associations, inverse_of: :item, class_name: 'RoomService::Item::ChoiceAssociation',
+           foreign_key: 'room_service_item_id', dependent: :destroy
+  has_many :choices, through: :item_choice_associations
   has_many :cart_items, inverse_of: :item, class_name: 'RoomService::CartItem',
            foreign_key: 'room_service_item_id', dependent: :destroy
 
@@ -15,9 +15,9 @@ class RoomService::Item < ApplicationRecord
   validates_length_of :short_description, maximum: 100
   validates_numericality_of :price, greater_than_or_equal_to: 0
 
-  delegate :available?, to: 'category_section', allow_nil: true
-  delegate :available_from, to: 'category_section', allow_nil: true
-  delegate :available_until, to: 'category_section', allow_nil: true
-  delegate :available_from_local, to: 'category_section', allow_nil: true
-  delegate :available_until_local, to: 'category_section', allow_nil: true
+  delegate :available?, to: :category_section, allow_nil: true
+  delegate :available_from, to: :category_section, allow_nil: true
+  delegate :available_until, to: :category_section, allow_nil: true
+  delegate :available_from_local, to: :category_section, allow_nil: true
+  delegate :available_until_local, to: :category_section, allow_nil: true
 end
--- spec/models/room_service/item_spec.rb
+++ spec/models/room_service/item_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 describe RoomService::Item do
-  it { should belong_to(:category_section).
-    counter_cache('room_service_items_count').
-    inverse_of(:items).
-    with_foreign_key('room_service_category_section_id') }
-  it { should have_many(:tag_associations).inverse_of(:item) }
+  it { should belong_to(:category_section).counter_cache('room_service_items_count').inverse_of(:items) }
+  it { should have_many(:tag_associations).inverse_of(:item).dependent(:destroy) }
   it { should have_many(:tags).through(:tag_associations) }
-  it { should have_many(:choice_associations).inverse_of(:item) }
-  it { should have_many(:choices).through(:choice_associations) }
+  it { should have_many(:item_choice_associations).inverse_of(:item).dependent(:destroy) }
+  it { should have_many(:choices).through(:item_choice_associations) }
   it { should have_many(:cart_items).inverse_of(:item).dependent(:destroy) }
 
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_most(50) }
   it { should validate_length_of(:short_description).is_at_most(100) }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+
+  it { should delegate_method(:available?).to(:category_section) }
+  it { should delegate_method(:available_from).to(:category_section) }
+  it { should delegate_method(:available_until).to(:category_section) }
+  it { should delegate_method(:available_from_local).to(:category_section) }
+  it { should delegate_method(:available_until_local).to(:category_section) }
 end
