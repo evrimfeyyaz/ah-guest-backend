03b63a523a826049c3a418d8d7ba150ccbb4c56e
Evrim P <evrimfeyyaz@gmail.com>
2017-09-17 13:52:59 +0300

    Refactor the Item::Choice model and its tests.

--- spec/models/room_service/item/choice_spec.rb
+++ spec/models/room_service/item/choice_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe RoomService::Item::Choice do
-  it { should have_many(:options).dependent(:destroy).inverse_of(:choice) }
+  it { should have_many(:options).inverse_of(:choice).dependent(:destroy) }
   it { should belong_to(:default_option) }
   it { should have_many(:item_choice_associations).inverse_of(:choice) }
   it { should have_many(:items).through(:item_choice_associations) }
@@ -9,21 +9,23 @@ describe RoomService::Item::Choice do
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_most(50) }
 
-  describe '#options' do
-    it 'is not valid when there are less than two options' do
+  # On why we're not using a shoulda matcher for ensuring options has at least two elements:
+  # https://github.com/thoughtbot/shoulda-matchers/issues/1007
+  describe 'options has a minimum of two elements validation' do
+    it 'adds a validation error when there are less than two options' do
       subject.options = []
 
       subject.validate
 
-      expect(subject.errors.details[:options]).to include({ error: :too_short, count: 2 })
+      expect(subject).to have_validation_error(:too_short).on(:options)
     end
 
-    it 'is valid when there is at least two options' do
+    it 'does not add a validation error when there is at least two options' do
       subject.options = build_list(:room_service_item_choice_option, 2)
 
       subject.validate
 
-      expect(subject.errors.details[:options]).not_to include({ error: :too_short, count: 2 })
+      expect(subject).not_to have_validation_error(:too_short).on(:options)
     end
   end
 end
