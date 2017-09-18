330e265f15b7e59c0780c4a8efa43d98759c83ef
Evrim P <evrimfeyyaz@gmail.com>
2017-09-18 10:22:44 +0300

    Refactor the Category::Section tests.

--- spec/models/room_service/category/section_spec.rb
+++ spec/models/room_service/category/section_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe RoomService::Category::Section do
-  it { should belong_to(:category).inverse_of(:sections).with_foreign_key('room_service_category_id') }
-  it { should have_many(:items).inverse_of(:category_section).with_foreign_key('room_service_category_section_id').dependent(:destroy) }
+  it { should belong_to(:category).inverse_of(:sections) }
+  it { should have_many(:items).inverse_of(:category_section).dependent(:destroy) }
 
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_most(50) }
